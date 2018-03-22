a7efd91be103d66e308d75ef33d8254afd5e7178
= <=mongiam4>
2018-03-22 22:45:27 +0530

    smooth scroll js

---
+++ scroll.js
@@ -0,0 +1,186 @@
+
+
+
+		document.getElementById("manan-menu").addEventListener("click", function(){
+			console.log('manan++');
+			var element = document.getElementById("MANANM");
+			starty = pageYOffset;
+			endy = element.offsetTop;
+
+			if ((starty-endy)<0) {
+				var x = setInterval(function(){
+ 				window.scrollBy(0, 3);
+ 				if (endy<=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);			
+			}
+			else{
+				var x = setInterval(function(){
+ 				window.scrollBy(0, -3);
+ 				if (endy>=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);	
+			}
+		});
+
+
+		document.getElementById("RAJA").addEventListener("click", function(){
+			console.log('manan++');
+			var element = document.getElementById("RAJAg");
+			starty = pageYOffset;
+			endy = element.offsetTop;
+
+			if ((starty-endy)<0) {
+				var x = setInterval(function(){
+ 				window.scrollBy(0, 3);
+ 				if (endy<=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);			
+			}
+			else{
+				var x = setInterval(function(){
+ 				window.scrollBy(0, -3);
+ 				if (endy>=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);	
+			}
+		});
+
+		document.getElementById("HD").addEventListener("click", function(){
+			console.log('manan++');
+			var element = document.getElementById("HDd");
+			starty = pageYOffset;
+			endy = element.offsetTop;
+
+			if ((starty-endy)<0) {
+				var x = setInterval(function(){
+ 				window.scrollBy(0, 3);
+ 				if (endy<=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);			
+			}
+			else{
+				var x = setInterval(function(){
+ 				window.scrollBy(0, -3);
+ 				if (endy>=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);	
+			}
+		});
+
+		document.getElementById("AYUSH").addEventListener("click", function(){
+			console.log('manan++');
+			var element = document.getElementById("AYUSHa");
+			starty = pageYOffset;
+			endy = element.offsetTop;
+
+			if ((starty-endy)<0) {
+				var x = setInterval(function(){
+ 				window.scrollBy(0, 3);
+ 				if (endy<=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);			
+			}
+			else{
+				var x = setInterval(function(){
+ 				window.scrollBy(0, -3);
+ 				if (endy>=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);	
+			}
+		});
+
+
+
+
+document.getElementById("RAHUL").addEventListener("click", function(){
+			console.log('manan++');
+			var element = document.getElementById("RAHULg");
+			starty = pageYOffset;
+			endy = element.offsetTop;
+
+			if ((starty-endy)<0) {
+				var x = setInterval(function(){
+ 				window.scrollBy(0, 3);
+ 				if (endy<=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);			
+			}
+			else{
+				var x = setInterval(function(){
+ 				window.scrollBy(0, -3);
+ 				if (endy>=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);	
+			}
+		});
+
+
+
+document.getElementById("EKTA").addEventListener("click", function(){
+			console.log('manan++');
+			var element = document.getElementById("EKTAs");
+			starty = pageYOffset;
+			endy = element.offsetTop;
+
+			if ((starty-endy)<0) {
+				var x = setInterval(function(){
+ 				window.scrollBy(0, 3);
+ 				if (endy<=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);			
+			}
+			else{
+				var x = setInterval(function(){
+ 				window.scrollBy(0, -3);
+ 				if (endy>=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);	
+			}
+		});
+
+
+document.getElementById("VINAY").addEventListener("click", function(){
+			console.log('manan++');
+			var element = document.getElementById("VINAYt");
+			starty = pageYOffset;
+			endy = element.offsetTop;
+
+			if ((starty-endy)<0) {
+				var x = setInterval(function(){
+ 				window.scrollBy(0, 3);
+ 				if (endy<=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);			
+			}
+			else{
+				var x = setInterval(function(){
+ 				window.scrollBy(0, -3);
+ 				if (endy>=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);	
+			}
+		});
+
+
+document.getElementById("RITUJA").addEventListener("click", function(){
+			console.log('manan++');
+			var element = document.getElementById("RITUJAS");
+			starty = pageYOffset;
+			endy = element.offsetTop;
+
+			if ((starty-endy)<0) {
+				var x = setInterval(function(){
+ 				window.scrollBy(0, 3);
+ 				if (endy<=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);			
+			}
+			else{
+				var x = setInterval(function(){
+ 				window.scrollBy(0, -3);
+ 				if (endy>=pageYOffset) 
+ 					clearInterval(x);
+				}, 10);	
+			}
+		});
